ae46dbb3501d1e48af98db59f4a9a7b79f2e77e5
Pedro Furtado <pedro.nazare@pdcase.com.br>
2023-04-04 17:02:15 -0300

    first commit

---
+++ src/main/java/com/ppfurtado/jgcrud/JgCrudApplication.java
@@ -0,0 +1,13 @@
+package com.ppfurtado.jgcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JgCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JgCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ppfurtado/jgcrud/JgCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppfurtado.jgcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JgCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
