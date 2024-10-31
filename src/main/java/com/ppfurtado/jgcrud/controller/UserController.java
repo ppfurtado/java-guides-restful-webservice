7adc657849ba0badcc6ee1adc926fcdcf1b82808
Pedro Furtado <furtado.pedropaulo@gmail.com>
2024-10-30 23:22:40 -0300

    feat: details docs with swagger

--- src/main/java/com/ppfurtado/jgcrud/controller/UserController.java
+++ src/main/java/com/ppfurtado/jgcrud/controller/UserController.java
@@ -4,6 +4,9 @@ package com.ppfurtado.jgcrud.controller;
 import com.ppfurtado.jgcrud.dto.UserDto;
 import com.ppfurtado.jgcrud.entity.User;
 import com.ppfurtado.jgcrud.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,6 +15,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(
+        name = "CRUD JAVAGUIDES",
+        description = "CRUD REST API"
+)
 @RestController
 @AllArgsConstructor
 @RequestMapping("api/v1/users")
@@ -19,12 +26,24 @@ public class UserController {
 
     private UserService userService;
 
+    @Operation(
+            summary = "Create User REST API",
+            description = "EndPoint para criarção de user"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 CREATED"
+    )
     @PostMapping
     public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto user){
         UserDto savedUser = userService.createUser(user);
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Find User REST API",
+            description = "Endpoint para buscar user por id"
+    )
     @GetMapping("/{id}")
     public ResponseEntity<UserDto> findById(@PathVariable Long id){
         UserDto findUser = userService.findById(id);
--- src/main/java/com/ppfurtado/jgcrud/dto/UserDto.java
+++ src/main/java/com/ppfurtado/jgcrud/dto/UserDto.java
@@ -1,14 +1,27 @@
 package com.ppfurtado.jgcrud.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 
 public record UserDto(
+        @Schema(
+                description = "Id entity"
+        )
         Long id,
+        @Schema(
+                description = "Fist Name User"
+        )
         @NotEmpty
         String firstName,
+        @Schema(
+                description = "Last name User"
+        )
         @NotEmpty
         String lastName,
+        @Schema(
+                description = "email User"
+        )
         @NotEmpty
         @Email(message = "The email must be valid")
         String email
