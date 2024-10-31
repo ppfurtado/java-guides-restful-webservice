99a5d726fad7bb3f1803f31eadfecbb8d4dd54bd
Pedro Furtado <furtado.pedropaulo@gmail.com>
2024-10-30 23:22:06 -0300

    feat: details project

--- src/main/java/com/ppfurtado/jgcrud/JgCrudApplication.java
+++ src/main/java/com/ppfurtado/jgcrud/JgCrudApplication.java
@@ -1,9 +1,34 @@
 package com.ppfurtado.jgcrud;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Spring Boot Rest Api Documentation",
+                description = "Spring Boot Rest Api Documentation Demo",
+                version = "V1.0",
+                contact = @Contact(
+                        name = "Pedro Furtado",
+                        email = "furtado.pedropaulo@gmail.comn",
+                        url = "pedro-furtado.com.br"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "pedro-furtado.com.br/license"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Spring Boot Rest Api Documentation",
+                url = "pedro-furtado.com.br/user-management.html"
+        )
+)
 public class JgCrudApplication {
 
     public static void main(String[] args) {
